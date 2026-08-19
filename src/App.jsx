f5bca280071ec6897fbcee5851ab8ed3c59108a5
Fami <64388495+ZDPS121@users.noreply.github.com>
2026-08-19 15:25:08 +0000

    added Calculation api fix3

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
     setError('');
 
     try {
-      const params = "A=" + A + "&B=" + B;
+      const params = "a=" + A + "&b=" + B;
       const response = await fetch(
         `https://b0upvso9xk.execute-api.us-east-1.amazonaws.com/live?${params}`
       );
